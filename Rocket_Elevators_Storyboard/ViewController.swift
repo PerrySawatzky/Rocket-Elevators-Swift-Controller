29f67053d909a6b1a0e1f630de8d08e9ae3ecf67
PerrySawatzky <sawatzky.perry@gmail.com>
2021-08-17 23:13:59 -0400

    almost working broken elevator list

--- Rocket_Elevators_Storyboard/ViewController.swift
+++ Rocket_Elevators_Storyboard/ViewController.swift
@@ -11,14 +11,14 @@ struct APIResponse: Codable {
 //    let elevator: Elevator
     let id: Int
     let column_id: Int
-    let serial_number: String
-    let model: String
-    let elevator_type: String
-    let status: String
-    let information: String
-    let notes: String
-    let created_at: String
-    let updated_at: String
+    let serial_number: String?
+    let model: String?
+    let elevator_type: String?
+    let status: String?
+    let information: String?
+    let notes: String?
+    let created_at: String?
+    let updated_at: String?
 }
 
 //struct Elevator: Codable {
@@ -34,16 +34,90 @@ struct APIResponse: Codable {
 //    let updated_at: String
 //}
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    
+//    var result: APIResponse?
     
     let urlString = "https://whispering-tundra-91467.herokuapp.com/api/elevators/notInOperation"
     
     @IBOutlet var label: UILabel!
     
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getData()
+        //getData must be first since it populates the elevatorList array.
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
         NotificationCenter.default.addObserver(self, selector: #selector(didGetNotificaiton(_:)), name: Notification.Name("text1"), object: nil)
+        
+    }
+    
+    
+    public var elevatorArray = [APIResponse]()
+    
+    func getData(){
+        
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+//            print(data)
+            do {
+                let jsonResult = try JSONDecoder().decode([APIResponse].self, from: data)
+//                let str = String(decoding: data, as: UTF8.self)
+
+//                print(jsonResult)
+                for elevator in jsonResult {
+                    self.elevatorArray.append(elevator)
+                    print(elevator.id)
+                }
+//                print(jsonResult.count)
+            }
+            catch {
+                print(error, "this error")
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+    //tableview
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        let elevator = "Elevator 1"
+    
+        return elevator
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("numberOfRowsInSection", elevatorArray.count)
+        let model = elevatorArray.count
+        return model
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("cellForRowAt", elevatorArray.count)
+        let model = self.elevatorArray[indexPath.row].status
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model
+        return cell
     }
     
     @objc func didGetNotificaiton(_ notification: Notification) {
@@ -57,28 +131,7 @@ class ViewController: UIViewController {
         present(vc,animated: true)
     }
     
-    func getData(){
-        guard let url = URL(string: urlString) else {
-            return
-        }
-        let task = URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data, error == nil else {
-                return
-            }
-            do {
-                let jsonResult = try JSONDecoder().decode([APIResponse].self, from: data)
-                
-                for elevator in jsonResult {
-                    print(elevator.id)
-                }
-//                print(jsonResult.count)
-            }
-            catch {
-                print(error)
-            }
-        }
-        task.resume()
-    }
+
 
 }
 
